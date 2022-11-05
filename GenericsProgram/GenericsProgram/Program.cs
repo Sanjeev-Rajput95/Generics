e226c843f5b3d6c35febcedd03cf3e381f7b6b8e
sanjeev <sanjeevsingh.77d@gmail.com>
2022-11-05 22:57:21 +0530

    Print array of Integer, Double and Character

---
+++ GenericsProgram/GenericsProgram/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace GenericsProgram
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("-------- Welcome To Generics Problem ---------");
+
+            // GenericsProgram genericsProgram = new GenericsProgram();
+
+            int[] intArray = { 1, 2, 3, 4, 5 };
+            double[] doubleArray = { 1.1, 2.2, 3.3, 4.4 };
+            char[] charArray = { 'H', 'S', 'E', 'L', 'L', 'O' };
+
+            Generics.toPrint(intArray);
+            Generics.toPrint(doubleArray);
+            Generics.toPrint(charArray);
+        }
+    }
+}
