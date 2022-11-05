06a9f6abb48dd7651a5e1b460f3e7a0ffb8895b6
sanjeev <sanjeevsingh.77d@gmail.com>
2022-11-05 23:11:14 +0530

    Print array of Integer, Double and Character Using Generics Method

---
+++ GenericsProgram/GenericsProgram/GenericsProgram.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generics
+{
+    public class GenericsProgram
+    {
+        public static void toPrint<T>(T[] inputArray)
+        {
+            foreach (var element in inputArray)
+            {
+                System.Console.WriteLine(element);
+            }
+            Console.WriteLine("------------------------  ");
+        }
+    }
+}
--- GenericsProgram/GenericsProgram/Program.cs
+++ GenericsProgram/GenericsProgram/Program.cs
@@ -1,20 +1,19 @@
-﻿namespace GenericsProgram
+﻿namespace Generics
 {
-    internal class Program
+    public class Program
     {
         static void Main(string[] args)
         {
             Console.WriteLine("-------- Welcome To Generics Problem ---------");
 
-            // GenericsProgram genericsProgram = new GenericsProgram();
 
             int[] intArray = { 1, 2, 3, 4, 5 };
             double[] doubleArray = { 1.1, 2.2, 3.3, 4.4 };
             char[] charArray = { 'H', 'S', 'E', 'L', 'L', 'O' };
 
-            Generics.toPrint(intArray);
-            Generics.toPrint(doubleArray);
-            Generics.toPrint(charArray);
+            GenericsProgram.toPrint(intArray);
+            GenericsProgram.toPrint(doubleArray);
+            GenericsProgram.toPrint(charArray);
         }
     }
 }
