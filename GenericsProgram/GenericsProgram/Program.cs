01a51df50ab8d34d0752688da7533dfa00d97f04
sanjeev <sanjeevsingh.77d@gmail.com>
2022-11-07 23:46:14 +0530

    Check max string

--- GenericsProgram/GenericsProgram/Program.cs
+++ GenericsProgram/GenericsProgram/Program.cs
@@ -7,9 +7,15 @@
             Console.WriteLine("-------- Welcome To Generics Problem ---------");
 
 
+            int intArray = MaxNumberCheck.MaxIntegerNumber(23, 42, 12);
+            Console.WriteLine("\nMax integer number is: " + intArray);
 
             double output = MaxNumberCheck.MaxIntegerNumber(33.2, 23.4, 12.5);
-            Console.WriteLine("\nMax integer number is: " + output);
+            Console.WriteLine("\nMax floating number is: " + output);
+
+            string strArr = MaxNumberCheck.MaxIntegerNumber("Anubhav","Sanjeev","Ravi");
+            Console.WriteLine("\nMax string is: " + strArr);
+
 
         }
     }
