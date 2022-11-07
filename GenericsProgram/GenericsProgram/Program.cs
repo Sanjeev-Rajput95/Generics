56fc247b4f0179bc017a78ebbd1fd16b9bfd2da0
sanjeev <sanjeevsingh.77d@gmail.com>
2022-11-07 23:28:58 +0530

    Check Max Value

--- GenericsProgram/GenericsProgram/Program.cs
+++ GenericsProgram/GenericsProgram/Program.cs
@@ -7,15 +7,10 @@
             Console.WriteLine("-------- Welcome To Generics Problem ---------");
 
 
-            int[] intArray = { 1, 2, 3, 4, 5 };
-            double[] doubleArray = { 1.1, 2.2, 3.3, 4.4 };
-            char[] charArray = { 'H', 'S', 'E', 'L', 'L', 'O' };
 
-            new PrintArray<int>(intArray).toPrint();
-            new PrintArray<double>(doubleArray).toPrint();
-            new PrintArray<char>(charArray).toPrint();
+            int output = MaxNumberCheck.MaxIntegerNumber(8347, 8778, 884);
+            Console.WriteLine("\nMax integer number is: " + output);
 
-        
         }
     }
 }
