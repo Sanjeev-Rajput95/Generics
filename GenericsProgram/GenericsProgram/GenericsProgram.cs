d24dc1130faeae5e74b2b27a235c591d9f95265a
sanjeev <sanjeevsingh.77d@gmail.com>
2022-11-08 11:29:17 +0530

    Sorting the method

--- GenericsProgram/GenericsProgram/GenericsProgram.cs
+++ GenericsProgram/GenericsProgram/GenericsProgram.cs
@@ -6,34 +6,36 @@ using System.Threading.Tasks;
 
 namespace Generics
 {
-    public class MaxNumberCheck<T>where T : IComparable
-    {
-        public T val1, val2, val3;
-
-        public MaxNumberCheck(T val1, T val2, T val3)
+        public class GenericMaximum<T> where T : IComparable
         {
-            this.val1 = val1;
-            this.val2 = val2;
-            this.val3 = val3;
+            public T[] value;
+            public GenericMaximum(T[] value)
+            {
+                this.value = value;
+            }
+
+            public T[] Sort(T[] values)
+            {
+                Array.Sort(values);
+                return values;
+            }
+
+            public T MaxValue(params T[] values)
+            {
+                var sorted_values = Sort(values);
+                return sorted_values[^1];
+            }
+
+            public T MaxMethod()
+            {
+                var max = MaxValue(this.value);
+                return max;
+            }
+            public void PrintMaxValue()
+            {
+                var max = MaxValue(this.value);
+                Console.WriteLine( "Maximum value is "+ max);
+            }
         }
-
-        public static T MaxIntegerNumber(T val1, T val2, T val3)
-        {
-            if (val1.CompareTo(val2) > 0 && val1.CompareTo(val3) > 0 || val1.CompareTo(val2) >= 0 && val1.CompareTo(val3) > 0 || val1.CompareTo(val2) > 0 && val1.CompareTo(val3) >= 0)
-            {
-                return val1;
-            }
-            if (val2.CompareTo(val1) > 0 && val2.CompareTo(val3) > 0 || val2.CompareTo(val1) >= 0 && val2.CompareTo(val3) > 0 || val2.CompareTo(val1) > 0 && val2.CompareTo(val3) >= 0)
-            {
-                return val2;
-            }
-
-            if (val3.CompareTo(val1) > 0 && val3.CompareTo(val1) > 0 || val3.CompareTo(val1) >= 0 && val3.CompareTo(val1) > 0 || val3.CompareTo(val1) > 0 && val3.CompareTo(val1) >= 0)
-            {
-                return val3;
-            }
-            return val1;
-        }
-
-    }
+    
 }
